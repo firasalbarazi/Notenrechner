6faff3277b7fd782462f3b4c0a749c1e7b23ca7c
firasalbarazi <albaraziferas@gmail.com>
2022-01-27 12:55:16 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+let n1, n2, n3, n4, n5;
+let summe = 0;
+let ergebnis;
+let anzahlNoten = 0;
+function tfAuslesen () {
+    summe = 0;
+    ergebnis;
+    anzahlNoten = 0;
+    n1 = parseInt(document.getElementById("tfNote1").value);
+    n2 = parseInt(document.getElementById("tfNote2").value);
+    n3 = parseInt(document.getElementById("tfNote3").value);
+    n4 = parseInt(document.getElementById("tfNote4").value);
+    n5 = parseInt(document.getElementById("tfNote5").value);
+
+    console.log(n1 + ";" + n2 + ";" + n3 + ";" + n4 + ";" + n5);
+
+    eingabeUeberpruefen(n1);
+    eingabeUeberpruefen(n2);
+    eingabeUeberpruefen(n3);
+    eingabeUeberpruefen(n4);
+    eingabeUeberpruefen(n5);
+
+    ergebnis = summe / anzahlNoten;
+    document.getElementById("ausgabe").innerHTML = ergebnis;
+    document.getElementById("ausgabe").style.color = "Red"
+
+    if (ergebnis > 8)
+    {document.getElementById("ausgabe").style.color = "Green"} else
+    if (ergebnis > 5)
+    {document.getElementById("ausgabe").style.color = "yellow"} else
+    if (ergebnis < 3)
+    {document.getElementById("ausgabe").style.color = "Red"}
+
+}
+
+function eingabeUeberpruefen(punkte) {
+    if (isNaN(punkte)){
+        console.log("Keine Zahl");
+    } else {
+    anzahlNoten = anzahlNoten + 1;
+    summe = summe + punkte;
+        }
+
+
+}
